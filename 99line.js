4a3d19644ccaf1b3badc1164ce7dcb5658c1726a
harrison-allinder <62095794+harrison-allinder@users.noreply.github.com>
2020-05-11 19:29:18 -0500

    Add files via upload

---
+++ 99line.js
@@ -0,0 +1,22 @@
+var friends = ["Jayme", "Harrison", "Ryan", "Austin", "John",];
+
+
+function codeinthefile(friends) {
+    let word = "lines"
+    let count = 99;
+    while (count >0) {
+        if (count == 1) {
+            word = "line";
+        }
+        console.log(count+ " " + word + " of code in the file,");
+        console.log(count + " " + word + " of code;");
+        console.log(friends + " strikes one out, clears it all out,");
+        if (count == 1) {
+            console.log("No more lines of code in the file!");
+        }
+        count = count - 1
+    }
+}
+for(var i = 0; i <friends.length; i++) {
+    codeinthefile(friends[i])
+}
